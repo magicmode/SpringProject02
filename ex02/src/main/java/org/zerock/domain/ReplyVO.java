e70fa8e43b19dada43a4c14cc5ca96b0564eca4e
magicmode <magicmode0@gmail.com>
2016-10-22 13:06:04 +0900

    스프링 MVC II 24 - 댓글 처리와 REST 2 ( 1. 테이블 설정 ~  7. 서비스 인터페이스 구현 )

---
+++ ex02/src/main/java/org/zerock/domain/ReplyVO.java
@@ -0,0 +1,56 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+public class ReplyVO {
+	private Integer rno;
+	private Integer bno;
+	private String replytext;
+	private String replyer;
+	private Date regDate;
+	private Date updateDate;
+	
+	public Integer getRno() {
+		return rno;
+	}
+	public void setRno(Integer rno) {
+		this.rno = rno;
+	}
+	public Integer getBno() {
+		return bno;
+	}
+	public void setBno(Integer bno) {
+		this.bno = bno;
+	}
+	public String getReplytext() {
+		return replytext;
+	}
+	public void setReplytext(String replytext) {
+		this.replytext = replytext;
+	}
+	public String getReplyer() {
+		return replyer;
+	}
+	public void setReplyer(String replyer) {
+		this.replyer = replyer;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+	@Override
+	public String toString() {
+		return "ReplyVO [rno=" + rno + ", bno=" + bno + ", replytext=" + replytext + ", replyer=" + replyer
+				+ ", regDate=" + regDate + ", updateDate=" + updateDate + "]";
+	}
+	
+}
